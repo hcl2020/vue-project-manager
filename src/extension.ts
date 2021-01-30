508fb36ad98925b19e22b6f8cdc1b109d67d6b8e
hcl2020 <907953320@qq.com>
2021-01-30 18:04:27 +0800

    chore: 新增demo logo，发布第一个预览版

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,10 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('vue-project-manager 扩展已激活');
   let workspaceRoot = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath!;
 
-  let disposable = vscode.commands.registerCommand('vue-project-manager.helloWorld', () => {
-    vscode.window.showInformationMessage('Hello World from Vue Project Manager!');
-  });
-  context.subscriptions.push(disposable);
+  // let disposable = vscode.commands.registerCommand('vue-project-manager.helloWorld', () => {
+  //   vscode.window.showInformationMessage('Hello World from Vue Project Manager!');
+  // });
+  // context.subscriptions.push(disposable);
 
   const nodeDependenciesProvider = new NodeDependenciesProvider(workspaceRoot);
   vscode.window.registerTreeDataProvider('projectTreeView', nodeDependenciesProvider);
