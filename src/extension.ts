ccea2a533535ef3b248b77fb8e5a2a696d5d8a95
hcl2020 <907953320@qq.com>
2021-02-07 11:33:48 +0800

    feat: 优化配置文件初始化提示，增加配置文件创建功能

--- src/extension.ts
+++ src/extension.ts
@@ -12,29 +12,51 @@ export function activate(context: vscode.ExtensionContext) {
   let workspaceRequire = Module.createRequire(workspaceRootPath);
   // let workspaceRequire = __non_webpack_require__;
 
-  function loadConfig(): IConfig {
+  function loadConfig(): IConfig | undefined {
     try {
       // TODO: 类型检测
       delete workspaceRequire.cache[configFileUri.fsPath];
       return workspaceRequire(configFileUri.fsPath);
     } catch (e) {
-      vscode.window.showWarningMessage(`缺少配置文件 ${PROJECT_CONFIG_FILENAME}`);
+      console.log(e.message);
     }
-    return { folders: [] };
   }
 
   const config = loadConfig();
   const nodeDependenciesProvider = new NodeDependenciesProvider(workspaceRootPath);
-  const projectTreeDataProvider = new ProjectTreeDataProvider(workspaceRootPath, config);
+  const projectTreeDataProvider = new ProjectTreeDataProvider(workspaceRootPath, config || { folders: [] });
+
+  let refreshProject = function refreshProject() {
+    let config = loadConfig();
+    if (config) {
+      projectTreeDataProvider.refresh(config);
+      vscode.window.showInformationMessage('已执行刷新');
+    } else {
+      vscode.window.showWarningMessage(`缺少配置文件 ${PROJECT_CONFIG_FILENAME}`);
+    }
+  };
+
+  let createConfigFile = async function createConfigFile() {
+    try {
+      await vscode.workspace.fs.readFile(configFileUri); // TODO: 解析验证配置
+      console.warn('配置文件已经存在');
+    } catch (e) {
+      let fileContent = `
+module.exports = {
+  folders: [
+    { name: 'Root', path: '.' }
+  ]
+};`;
+      await vscode.workspace.fs.writeFile(configFileUri, Buffer.from(fileContent, 'utf8'));
+    }
+    refreshProject();
+  };
 
   context.subscriptions.push(
     vscode.window.registerTreeDataProvider('nodeDependenciesTreeView', nodeDependenciesProvider),
     vscode.window.registerTreeDataProvider('projectTreeView', projectTreeDataProvider),
-    vscode.commands.registerCommand('vpm.refreshProject', () => {
-      let config = loadConfig();
-      projectTreeDataProvider.refresh(config);
-      vscode.window.showInformationMessage('已执行刷新');
-    })
+    vscode.commands.registerCommand('vpm.refreshProject', refreshProject),
+    vscode.commands.registerCommand('vpm.createConfigFile', createConfigFile)
   );
 }
 
