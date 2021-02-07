4f524e3d97574e14196e62f54d9612c21d2fc58e
hcl2020 <907953320@qq.com>
2021-02-07 14:36:33 +0800

    feat: 优化node_modules依赖解析，增加单击跳转功能

--- src/NodeDependenciesProvider.ts
+++ src/NodeDependenciesProvider.ts
@@ -2,6 +2,15 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
+function pathExists(p: string): boolean {
+  try {
+    fs.accessSync(p);
+  } catch (err) {
+    return false;
+  }
+  return true;
+}
+
 export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
   constructor(private workspaceRoot: string) {}
 
@@ -9,23 +18,21 @@ export class NodeDependenciesProvider implements vscode.TreeDataProvider<Depende
     return element;
   }
 
-  getChildren(element?: Dependency): Thenable<Dependency[]> {
+  async getChildren(element?: Dependency): Promise<Dependency[]> {
     if (!this.workspaceRoot) {
-      vscode.window.showInformationMessage('No dependency in empty workspace');
-      return Promise.resolve([]);
+      // vscode.window.showInformationMessage('No dependency in empty workspace');
+      return [];
     }
 
     if (element) {
-      return Promise.resolve(
-        this.getDepsInPackageJson(path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json'))
-      );
+      return element.resourceUri?.fsPath ? this.getDepsInPackageJson(element.resourceUri?.fsPath) : [];
     } else {
       const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
-      if (this.pathExists(packageJsonPath)) {
-        return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
+      if (pathExists(packageJsonPath)) {
+        return this.getDepsInPackageJson(packageJsonPath);
       } else {
-        vscode.window.showInformationMessage('Workspace has no package.json');
-        return Promise.resolve([]);
+        // vscode.window.showInformationMessage('Workspace has no package.json');
+        return [];
       }
     }
   }
@@ -34,37 +41,43 @@ export class NodeDependenciesProvider implements vscode.TreeDataProvider<Depende
    * Given the path to package.json, read all its dependencies and devDependencies.
    */
   private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
-    if (this.pathExists(packageJsonPath)) {
+    if (pathExists(packageJsonPath)) {
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-      const toDep = (moduleName: string, version: string): Dependency => {
-        if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
-          let packageJsonUri = vscode.Uri.file(path.join(this.workspaceRoot, 'node_modules', moduleName));
-          return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed, packageJsonUri);
-        } else {
-          return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None);
-        }
+      const packageDirPath = path.dirname(packageJsonPath);
+
+      const getDeps = (deps: Record<string, string>, isDev = false): Dependency[] => {
+        const iconPath = new vscode.ThemeIcon(isDev ? 'github-alt' : 'github');
+
+        return Object.entries(deps).map(
+          ([moduleName, version]: [moduleName: string, version: string]): Dependency => {
+            let subPackageJsonPath = [
+              path.join(this.workspaceRoot, 'node_modules', moduleName, 'package.json'),
+              path.join(packageDirPath, 'node_modules', moduleName, 'package.json'),
+            ].find((p) => pathExists(p));
+
+            if (subPackageJsonPath) {
+              return new Dependency(
+                moduleName,
+                version,
+                vscode.TreeItemCollapsibleState.Collapsed,
+                iconPath,
+                vscode.Uri.file(subPackageJsonPath)
+              );
+            } else {
+              return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None, iconPath);
+            }
+          }
+        );
       };
 
-      const deps = packageJson.dependencies
-        ? Object.keys(packageJson.dependencies).map((dep) => toDep(dep, packageJson.dependencies[dep]))
-        : [];
-      const devDeps = packageJson.devDependencies
-        ? Object.keys(packageJson.devDependencies).map((dep) => toDep(dep, packageJson.devDependencies[dep]))
-        : [];
+      const deps = getDeps(packageJson.dependencies || {});
+      const devDeps = getDeps(packageJson.devDependencies || {}, true);
+
       return deps.concat(devDeps);
     } else {
       return [];
     }
-  }
-
-  private pathExists(p: string): boolean {
-    try {
-      fs.accessSync(p);
-    } catch (err) {
-      return false;
-    }
-    return true;
   }
 
   private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | null | void> = new vscode.EventEmitter<
@@ -83,10 +96,19 @@ class Dependency extends vscode.TreeItem {
     public readonly label: string,
     private version: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly iconPath?: vscode.ThemeIcon,
     public readonly resourceUri?: vscode.Uri
   ) {
     super(label, collapsibleState);
     this.tooltip = `${this.label}-${this.version}`;
-    this.description = this.version;
+    this.description = this.resourceUri?.fsPath;
+
+    if (this.resourceUri) {
+      this.command = {
+        title: 'this.label',
+        command: 'vscode.open',
+        arguments: [this.resourceUri],
+      };
+    }
   }
 }
